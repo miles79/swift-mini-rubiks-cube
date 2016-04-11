43b6bb1ae9bb7a803faa753a3fc339fe169cdef4
miles79 <milesdmcleod@gmail.com>
2016-04-11 05:33:39 -0700

    Cleaned up some stuff

---
+++ RubiksCube.swift
@@ -0,0 +1,125 @@
+//
+//  RubiksCube.swift
+//  SceneKitDemo
+//
+//  Created by Miles McLeod on 2016-04-11.
+//  Copyright © 2016 Miles McLeod. All rights reserved.
+//
+
+import SceneKit
+
+class RubiksCube : SCNNode {
+    
+    let cubeWidth:Float = 0.95
+    let spaceBetweenCubes:Float = 0.05
+    
+    enum CubeSide {
+        case North
+        case South
+        case East
+        case West
+        case Top
+        case Bottom
+    }
+    
+    override init() {
+        
+        super.init()
+        
+        //define material used for making the rubiks cube
+        let greenMaterial = SCNMaterial()
+        greenMaterial.diffuse.contents = UIColor.greenColor()
+        
+        let redMaterial = SCNMaterial()
+        redMaterial.diffuse.contents = UIColor.redColor()
+        
+        let blueMaterial = SCNMaterial()
+        blueMaterial.diffuse.contents = UIColor.blueColor()
+        
+        let yellowMaterial = SCNMaterial()
+        yellowMaterial.diffuse.contents = UIColor.yellowColor()
+        
+        let whiteMaterial = SCNMaterial()
+        whiteMaterial.diffuse.contents = UIColor.whiteColor()
+        
+        let orangeMaterial = SCNMaterial()
+        orangeMaterial.diffuse.contents = UIColor.orangeColor()
+        
+        let blackMaterial = SCNMaterial()
+        blackMaterial.diffuse.contents = UIColor.blackColor()
+        
+        // create the cube
+        let cubeOffsetDistance = self.cubeOffsetDistance()
+        
+        var xPos:Float = -cubeOffsetDistance
+        var yPos:Float = -cubeOffsetDistance
+        var zPos:Float = -cubeOffsetDistance
+        for i in 0..<2 {
+            for j in 0..<2 {
+                for k in 0..<2 {
+                    let cubeGeometry = SCNBox(width: CGFloat(cubeWidth), height: CGFloat(cubeWidth), length: CGFloat(cubeWidth), chamferRadius: 0)
+                    if i == 0 && j == 0 {
+                        cubeGeometry.materials = (k % 2 == 0) ? [blackMaterial, blackMaterial, blueMaterial, orangeMaterial, blackMaterial, whiteMaterial] : [blackMaterial, redMaterial, blueMaterial, blackMaterial, blackMaterial, whiteMaterial]
+                    }
+                    if i == 0 && j == 1 {
+                        cubeGeometry.materials = (k % 2 == 0) ? [blackMaterial, blackMaterial, blueMaterial, orangeMaterial, yellowMaterial, blackMaterial] : [blackMaterial, redMaterial, blueMaterial, blackMaterial, yellowMaterial, blackMaterial]
+                    }
+                    if i == 1 && j == 0 {
+                        cubeGeometry.materials = (k % 2 == 0) ? [greenMaterial, blackMaterial, blackMaterial, orangeMaterial, blackMaterial, whiteMaterial] : [greenMaterial, redMaterial, blackMaterial, blackMaterial, blackMaterial, whiteMaterial]
+                    }
+                    if i == 1 && j == 1 {
+                        cubeGeometry.materials = (k % 2 == 0) ? [greenMaterial, blackMaterial, blackMaterial, orangeMaterial, yellowMaterial, blackMaterial] : [greenMaterial, redMaterial, blackMaterial, blackMaterial, yellowMaterial, blackMaterial]
+                    }
+                    
+                    
+                    let cube = SCNNode(geometry: cubeGeometry)
+                    cube.position = SCNVector3(x: xPos, y: yPos, z: zPos)
+                    xPos += cubeWidth + spaceBetweenCubes
+                    self.addChildNode(cube)
+                }
+                xPos = -cubeOffsetDistance
+                yPos += cubeWidth + spaceBetweenCubes
+            }
+            xPos = -cubeOffsetDistance
+            yPos = -cubeOffsetDistance
+            zPos += cubeWidth + spaceBetweenCubes
+        }
+    }
+    
+    private func cubeOffsetDistance()->Float {
+        return (cubeWidth + spaceBetweenCubes) / 2
+    }
+    
+    func getSouthWall()->[SCNNode] {
+        let southWallNodes = self.childNodesPassingTest { (child, stop) -> Bool in
+            return child.position.z.nearlyEqual(self.cubeOffsetDistance(), epsilon: 0.01)
+        }
+        return southWallNodes
+    }
+    
+    func getNorthWall()->[SCNNode] {
+        let northWallNodes = self.childNodesPassingTest { (child, stop) -> Bool in
+            return child.position.z.nearlyEqual(-self.cubeOffsetDistance(), epsilon: 0.01)
+        }
+        return northWallNodes
+    }
+    
+    func isNorthWallSolved()->Bool {
+        let northWallNodes = getNorthWall()
+        print(northWallNodes[0].geometry?.materials[0].diffuse.contents)
+        let material = northWallNodes[0].geometry?.materials[0]
+        for i in 1..<northWallNodes.count {
+            if material != northWallNodes[i].geometry?.materials[0] {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isSolved() {
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- SceneKitDemo/ViewController.swift
+++ SceneKitDemo/ViewController.swift
@@ -12,7 +12,6 @@ import OpenGLES
 
 class ViewController: UIViewController {
     
-    // REVISE THIS FOR DEVICE ORIENTATION
     let screenSize: CGRect = UIScreen.mainScreen().bounds
     var screenWidth: Float!
     var screenHeight: Float!
@@ -21,20 +20,29 @@ class ViewController: UIViewController {
     var rootNode: SCNNode!
     var cameraNode: SCNNode!
     var lightNode: SCNNode!
-    var cubeWall: SCNNode!
+    var rubiksCube: RubiksCube!
 
     var beganPanHitResult: SCNHitTestResult!
     var beganPanNode: SCNNode!
+    
+    var animationLock = false
+    
+    enum CubeSide {
+        case North
+        case South
+        case East
+        case West
+        case Top
+        case Bottom
+    }
     
     var rotationAxis:SCNVector3!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /////////////////////////////////////
         screenWidth = Float(screenSize.width)
         screenHeight = Float(screenSize.height)
-        /////////////////////////////////////
         
         // set up the scene
         sceneView = SCNView(frame: self.view.frame)
@@ -42,68 +50,10 @@ class ViewController: UIViewController {
         sceneView.backgroundColor = UIColor(red: 0.3, green: 0.72, blue: 0.65, alpha: 1)
         self.view.addSubview(sceneView)
         rootNode = sceneView.scene!.rootNode
-        
-        //define material used for making the rubiks cube
-        let cubeWidth:CGFloat = 0.95
-        
-        let greenMaterial = SCNMaterial()
-        greenMaterial.diffuse.contents = UIColor.greenColor()
-        
-        let redMaterial = SCNMaterial()
-        redMaterial.diffuse.contents = UIColor.redColor()
-        
-        let blueMaterial = SCNMaterial()
-        blueMaterial.diffuse.contents = UIColor.blueColor()
-        
-        let yellowMaterial = SCNMaterial()
-        yellowMaterial.diffuse.contents = UIColor.yellowColor()
-        
-        let whiteMaterial = SCNMaterial()
-        whiteMaterial.diffuse.contents = UIColor.whiteColor()
-        
-        let orangeMaterial = SCNMaterial()
-        orangeMaterial.diffuse.contents = UIColor.orangeColor()
-        
-        let blackMaterial = SCNMaterial()
-        blackMaterial.diffuse.contents = UIColor.blackColor()
-        
-        // create rubiks cube and add it to the scene
-        cubeWall = SCNNode()
-        
-        var xPos:Float = -0.5
-        var yPos:Float = -0.5
-        var zPos:Float = -0.5
-        for i in 0..<2 {
-            for j in 0..<2 {
-                for k in 0..<2 {
-                    let cubeGeometry = SCNBox(width: cubeWidth, height: cubeWidth, length: cubeWidth, chamferRadius: 0)
-                    if i == 0 && j == 0 {
-                    cubeGeometry.materials = (k % 2 == 0) ? [blackMaterial, blackMaterial, blueMaterial, orangeMaterial, blackMaterial, whiteMaterial] : [blackMaterial, redMaterial, blueMaterial, blackMaterial, blackMaterial, whiteMaterial]
-                    }
-                    if i == 0 && j == 1 {
-                        cubeGeometry.materials = (k % 2 == 0) ? [blackMaterial, blackMaterial, blueMaterial, orangeMaterial, yellowMaterial, blackMaterial] : [blackMaterial, redMaterial, blueMaterial, blackMaterial, yellowMaterial, blackMaterial]
-                    }
-                    if i == 1 && j == 0 {
-                        cubeGeometry.materials = (k % 2 == 0) ? [greenMaterial, blackMaterial, blackMaterial, orangeMaterial, blackMaterial, whiteMaterial] : [greenMaterial, redMaterial, blackMaterial, blackMaterial, blackMaterial, whiteMaterial]
-                    }
-                    if i == 1 && j == 1 {
-                        cubeGeometry.materials = (k % 2 == 0) ? [greenMaterial, blackMaterial, blackMaterial, orangeMaterial, yellowMaterial, blackMaterial] : [greenMaterial, redMaterial, blackMaterial, blackMaterial, yellowMaterial, blackMaterial]
-                    }
  
-
-                    let cube = SCNNode(geometry: cubeGeometry)
-                    cube.position = SCNVector3(x: xPos, y: yPos, z: zPos)
-                    xPos += Float(cubeWidth) + 0.05
-                    cubeWall.addChildNode(cube)
-                }
-                xPos = -0.5
-                yPos += Float(cubeWidth) + 0.05
-            }
-            xPos = -0.5
-            yPos = -0.5
-            zPos += Float(cubeWidth) + 0.05
-        }
-        rootNode.addChildNode(cubeWall)
+        // create and add rubiks cube to the scene
+        rubiksCube = RubiksCube()
+        rootNode.addChildNode(rubiksCube)
         
         // create and add camera to the scene
         let camera = SCNCamera()
@@ -114,15 +64,22 @@ class ViewController: UIViewController {
         
         // place the camera
         cameraNode.position = SCNVector3Make(0, 0, 0);
-        cameraNode.pivot = SCNMatrix4MakeTranslation(0, 0, -15); //the -15 here will become the rotation radius
+        cameraNode.pivot = SCNMatrix4MakeTranslation(0, 0, -10); //the -15 here will become the rotation radius
         
         // set gesture recognizers
+        let rotationRecognizer = UIRotationGestureRecognizer()
+        rotationRecognizer.addTarget(self, action: "sceneRotated:")
+        
         let panRecognizer = UIPanGestureRecognizer()
         panRecognizer.addTarget(self, action: "scenePanned:")
-        sceneView.gestureRecognizers = [panRecognizer]
+        
+        sceneView.gestureRecognizers = [rotationRecognizer, panRecognizer]
     }
     
     // gesture handlers
+    func sceneRotated(recognizer: UIRotationGestureRecognizer) {
+    }
+    
     func scenePanned(recognizer: UIPanGestureRecognizer) {
         let location = recognizer.locationInView(sceneView)
         let hitResults = sceneView.hitTest(location, options: nil)
@@ -147,18 +104,23 @@ class ViewController: UIViewController {
         }
         
         // 1 finger on cube: rotate cube section
-        if recognizer.numberOfTouches() == 1 && hitResults.count > 0 && recognizer.state == UIGestureRecognizerState.Began {
+        if recognizer.numberOfTouches() == 1
+            && hitResults.count > 0
+            && recognizer.state == UIGestureRecognizerState.Began
+            && beganPanNode == nil {
+
             beganPanHitResult = hitResults[0]
-            print(beganPanHitResult.worldCoordinates)
             beganPanNode = hitResults[0].node
-                        print(beganPanNode.position);
         }
         
-        else if recognizer.state == UIGestureRecognizerState.Ended && beganPanNode != nil {
+        else if recognizer.state == UIGestureRecognizerState.Ended && beganPanNode != nil && animationLock == false {
+            animationLock = true
+            
             let locationView = recognizer.locationInView(sceneView);
             let projectedOrigin = sceneView.projectPoint(beganPanHitResult.worldCoordinates);
             
             let estimatedPoint = sceneView.unprojectPoint(SCNVector3( Float(locationView.x), Float(locationView.y), projectedOrigin.z) );
+
             var plane = "?";
             var direction = 1;
             
@@ -170,31 +132,36 @@ class ViewController: UIViewController {
             let absYDiff = abs(yDiff)
             let absZDiff = abs(zDiff)
             
-            var side = "?";
+            var side:CubeSide!;
             
             if beganPanHitResult.worldCoordinates.x.nearlyEqual(0.975, epsilon: 0.025) {
-                side = "EAST";
+                side = CubeSide.East
             }
             else if beganPanHitResult.worldCoordinates.x.nearlyEqual(-0.975, epsilon: 0.025) {
-                side = "WEST";
+                side = CubeSide.West
             }
             else if beganPanHitResult.worldCoordinates.y.nearlyEqual(0.975, epsilon: 0.025) {
-                side = "TOP";
+                side = CubeSide.Top
             }
             else if beganPanHitResult.worldCoordinates.y.nearlyEqual(-0.975, epsilon: 0.025) {
-                side = "BOTTOM";
+                side = CubeSide.Bottom
             }
             else if beganPanHitResult.worldCoordinates.z.nearlyEqual(0.975, epsilon: 0.025) {
-                side = "SOUTH";
+                side = CubeSide.South
             }
             else if beganPanHitResult.worldCoordinates.z.nearlyEqual(-0.975, epsilon: 0.025) {
-                side = "NORTH";
+                side = CubeSide.North
+            }
+            else {
+                self.animationLock = false;
+                self.beganPanNode = nil;
+                return
             }
             
-            if side == "EAST" || side == "WEST" {
+            if side == CubeSide.East || side == CubeSide.West {
                 if absYDiff > absZDiff {
                     plane = "Y";
-                    if side == "EAST" {
+                    if side == CubeSide.East {
                         direction = yDiff > 0 ? -1 : 1;
                     }
                     else {
@@ -203,7 +170,7 @@ class ViewController: UIViewController {
                 }
                 else {
                     plane = "Z";
-                    if side == "EAST" {
+                    if side == CubeSide.East {
                         direction = zDiff > 0 ? 1 : -1;
                     }
                     else {
@@ -211,10 +178,10 @@ class ViewController: UIViewController {
                     }
                 }
             }
-            else if side == "TOP" || side == "BOTTOM" {
+            else if side == CubeSide.Top || side == CubeSide.Bottom {
                 if absXDiff > absZDiff {
                     plane = "X";
-                    if side == "TOP" {
+                    if side == CubeSide.Top {
                         direction = xDiff > 0 ? 1 : -1;
                     }
                     else {
@@ -223,7 +190,7 @@ class ViewController: UIViewController {
                 }
                 else {
                     plane = "Z"
-                    if side == "TOP" {
+                    if side == CubeSide.Top {
                         direction = zDiff > 0 ? -1 : 1;
                     }
                     else {
@@ -231,10 +198,10 @@ class ViewController: UIViewController {
                     }
                 }
             }
-            else if side == "SOUTH" || side == "NORTH" {
+            else if side == CubeSide.South || side == CubeSide.North {
                 if absXDiff > absYDiff {
                     plane = "X";
-                    if side == "SOUTH" {
+                    if side == CubeSide.South {
                         direction = xDiff > 0 ? -1 : 1;
                     }
                     else {
@@ -243,7 +210,7 @@ class ViewController: UIViewController {
                 }
                 else {
                     plane = "Y"
-                    if side == "SOUTH" {
+                    if side == CubeSide.South {
                         direction = yDiff > 0 ? 1 : -1;
                     }
                     else {
@@ -252,32 +219,30 @@ class ViewController: UIViewController {
                 }
             }
             
-            //
-            // START ANIMATING
-            //
-
             // get the nodes we want to rotate
-            let nodesToRotate =  cubeWall.childNodesPassingTest { (child, stop) -> Bool in
-                if ((side == "EAST" || side == "WEST") && plane == "Z")
-                    || ((side == "NORTH" || side == "SOUTH") && plane == "X") {
+            let nodesToRotate =  rubiksCube.childNodesPassingTest { (child, stop) -> Bool in
+                if ((side == CubeSide.East || side == CubeSide.West) && plane == "Z")
+                    || ((side == CubeSide.North || side == CubeSide.South) && plane == "X") {
                         self.rotationAxis = SCNVector3(0,1,0);
                     return child.position.y.nearlyEqual(self.beganPanNode.position.y, epsilon: 0.025)
                 }
-                if ((side == "EAST" || side == "WEST") && plane == "Y")
-                    || ((side == "TOP" || side == "BOTTOM") && plane == "X") {
+                if ((side == CubeSide.East || side == CubeSide.West) && plane == "Y")
+                    || ((side == CubeSide.Top || side == CubeSide.Bottom) && plane == "X") {
                         self.rotationAxis = SCNVector3(0,0,1);
                         return child.position.z.nearlyEqual(self.beganPanNode.position.z, epsilon: 0.025)
                 }
-                if ((side == "NORTH" || side == "SOUTH") && plane == "Y")
-                    || ((side == "TOP" || side == "BOTTOM") && plane == "Z") {
+                if ((side == CubeSide.North || side == CubeSide.South) && plane == "Y")
+                    || ((side == CubeSide.Top || side == CubeSide.Bottom) && plane == "Z") {
                         self.rotationAxis = SCNVector3(1,0,0);
                         return child.position.x.nearlyEqual(self.beganPanNode.position.x, epsilon: 0.025)
                 }
                 return false;
             }
             
-            // this shouldnt happen, so bounce out
+            // this shouldnt happen, so exit
             if nodesToRotate.count <= 0 {
+                self.animationLock = false;
+                self.beganPanNode = nil;
                 return
             }
             // add nodes we want to rotate to a parent node so that we can rotate relative to the root
@@ -288,20 +253,22 @@ class ViewController: UIViewController {
             }
             
             let rotationAngle = CGFloat(direction) * CGFloat(M_PI_2);
-            print(rotationAngle);
+
             // create action
-            let actionTest = SCNAction.rotateByAngle(rotationAngle, aroundAxis: self.rotationAxis, duration: 1)
+            let actionTest = SCNAction.rotateByAngle(rotationAngle, aroundAxis: self.rotationAxis, duration: 0.5)
             
             // apply action and remove nodes from container and add back to root
             container.runAction(actionTest, completionHandler: { () -> Void in
                 for node: SCNNode in nodesToRotate {
-                    let transform = node.parentNode!.convertTransform(node.transform, toNode: self.cubeWall)
+                    let transform = node.parentNode!.convertTransform(node.transform, toNode: self.rubiksCube)
                     node.removeFromParentNode()
                     node.transform = transform
-                    self.cubeWall.addChildNode(node)
+                    self.rubiksCube.addChildNode(node)
                 }
+                print(self.rubiksCube.isNorthWallSolved())
+                self.animationLock = false
+                self.beganPanNode = nil
             })
-            beganPanNode = nil;
         }
     }
     
